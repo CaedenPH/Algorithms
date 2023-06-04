667bba221b5a03d0610cf50ba720aa44d12157e0
CaedenPH <caedenperelliharris@gmail.com>
2023-06-04 14:44:05 +0100

    feat: Initial commit & 04/06/23

---
+++ Daily Challenges/2023/June/day_four.py
@@ -0,0 +1,56 @@
+"""
+Daily challenge for 04/06/2023
+> https://leetcode.com/problems/number-of-provinces/description/
+
+Time complexity:
+    O(n^2) - n is the number of cities
+    Only beats 5% of submissions
+
+Notes:
+    - Time taken was 43:28
+    - Poor solution, needs to be improved
+"""
+
+from typing import Dict, List
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        connections: Dict[int, List[int]] = {}
+        n = len(isConnected)
+
+        # Group connections
+        for i in range(n):
+            for j in range(n):
+                if i not in connections:
+                    connections[i] = []
+                if i != j and isConnected[i][j] == 1:
+                    connections[i].append(j)
+                
+        for i in list(connections.keys()).copy():  # 0
+            print(i, connections)
+            if i not in connections:
+                continue
+
+            cc = connections[i]  # [3]
+            # Find first occurance of i
+            for v in list(connections.keys()).copy():
+                if i in connections.get(v, []) and i != v:
+                    # Completely dump cc into this occurance
+                    connections[v].extend(cc)
+                    del connections[i]
+                    break
+                
+            for v in cc:
+                if i not in connections:
+                    continue
+                for j in list(connections.keys()).copy():
+                    if v in connections[j] and j != i:
+                        connections[j].extend(cc)
+                        del connections[i]
+                        break
+
+        return len(connections)
+        
+            
+if __name__ == "__main__":
+    Solution().findCircleNum([[1,1,0,0,0,0,0,1,0,0,0,0,0,0,0],[1,1,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,1,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,1,0,1,1,0,0,0,0,0,0,0,0],[0,0,0,0,1,0,0,0,0,1,1,0,0,0,0],[0,0,0,1,0,1,0,0,0,0,1,0,0,0,0],[0,0,0,1,0,0,1,0,1,0,0,0,0,1,0],[1,0,0,0,0,0,0,1,1,0,0,0,0,0,0],[0,0,0,0,0,0,1,1,1,0,0,0,0,1,0],[0,0,0,0,1,0,0,0,0,1,0,1,0,0,1],[0,0,0,0,1,1,0,0,0,0,1,1,0,0,0],[0,0,0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,1,0,1,0,0,0,0,1,0],[0,0,0,0,0,0,0,0,0,1,0,0,0,0,1]])
---
+++ README.md
@@ -0,0 +1,23 @@
+## Leetcode
+
+This is a repository where I will be saving my solutions to the problems I solve on Leetcode. 
+I aim to solve the daily challenge everyday and also solve other problems as well.
+
+### Languages
+As I start this repository, I will be using Python and Javascript to solve the problems. As I get more comfortable solving leetcode problems, I will start using using C++ and Java as well.
+
+### Structure
+The structure of the repository is as follows:
+```
+├── Daily Challenges
+│   ├── 2023
+│   │   ├── June
+│   │   │   ├── day_one.py
+
+├── Other Problems
+│   ├── 2023
+```
+
+### Disclaimer
+The solutions provided here are not the only solutions to the problems. There are many other ways to solve the problems. The solutions provided here are the ones I came up with. I will try to provide the time and space complexity of the solutions as well.
+My solutions may not be the most optimal solutions. I will try to improve them as I learn more about the problems and the languages I am using.
